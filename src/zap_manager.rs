2354679babfb1eef0c9bb80062135b97b2460189
Antony <antonyburrows314@gmail.com>
2026-01-20 19:42:01 +0800

    Fix ABI path to use relative path from workspace root
    Changed absolute path to relative path for zap_manager-abi.json
    to enable builds in Docker containers and CI environments.

--- src/zap_manager.rs
+++ src/zap_manager.rs
@@ -1,11 +1,9 @@
 use fuels::prelude::*;
 
-
-// #[cfg(not(debug_assertions))]
 abigen!(
     // ZapManager
     Contract(
         name = "ZapManager",
-        abi = "/media/catsper/data6/L3LABS_ORG/zap-contracts-development/programs/zap_manager/out/release/zap_manager-abi.json"
+        abi = "zap-contracts-development/programs/zap_manager/out/release/zap_manager-abi.json"
     ),
 );
